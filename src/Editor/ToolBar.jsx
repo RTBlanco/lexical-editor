1a65d2ac5ba0ea3777fa59a2744a0ad01e3d5c5c
rtblanco <ronnytoribio1@hotmail.com>
2026-02-23 22:15:45 -0500

    rearragned the buttons

--- src/Editor/ToolBar.jsx
+++ src/Editor/ToolBar.jsx
@@ -1,5 +1,8 @@
-import { HeadingPlugin } from "./plugins/HeaderPlugin"
+import HeadingPlugin from "./plugins/HeaderPlugin"
 import ListsPlugin from "./plugins/ListsPlugin"
+import UnderLinedPlugin from "./plugins/UnderLinedPlugin"
+import BoldPlugin from "./plugins/BoldPlugin"
+import ItalicPlugin from "./plugins/ItalicPlugin"
 import "./toolbar.css"
 
 export default function ToolbarPlugin() {
@@ -7,6 +10,9 @@ export default function ToolbarPlugin() {
   return(
     <div className="toolbar-container">
       {/* <HeadingPlugin /> */}
+      <BoldPlugin />
+      <ItalicPlugin />
+      <UnderLinedPlugin />
       <ListsPlugin />
     </div>
   )
